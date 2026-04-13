ee3cfe49def2f208fd9e769cce5e1f073c7517ec
octs1st <octs1st>
2026-04-13 19:51:01 +0900

    FEAT : PDFLAG 오류 수정

--- 26-chatbot/06_PdfLag.py
+++ 26-chatbot/06_PdfLag.py
@@ -19,30 +19,19 @@ class MyEmbeddings(Embeddings):
         return self.embed_documents([text])[0]
     
 
-client = OpenAI(base_url="http://127.0.0.1:1234/v1", 
-                api_key="lm-studio"
-                )
+file_path = './files/bylaws.pdf'
+client = OpenAI(base_url="http://127.0.0.1:1234/v1", api_key="lm-studio")
 embeddings = MyEmbeddings(base_url="http://127.0.0.1:1234/v1")  
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, 
+                                               chunk_overlap=20,
+                                               separators=["\n\n", "\n", r"(?<=\. )", " ", ""], 
+                                               length_function=len)
 
-completion = client.chat.completions.create(
-    model="llama-3-korean-bllossom-8b",
-    temperature=0.7,
-    messages=[
-        {"role": "system", "content": "문장 끝날 때 이모티콘을 써줘"},
-        {"role": "user", "content": "동서대학교의 수업 연한은?"}
-  ],
-)
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20,
-                                               separators=["\n\n", "\n", "(?<=\. )", " ", ""], length_function=len)
-
-
-def embed_file(file):
-    with open('./files/bylaws.pdf', 'wb') as f:
-        f.write(file.read())
-    Loader = {'txt':TextLoader, 'pdf':PyPDFLoader}[file.name.split('.')[-1].lower()]
-    docs = Loader('./files/bylaws.pdf').load_and_split(text_splitter=text_splitter)
-    splits = text_splitter.split_documents(docs)
+def embed_file(path):
+    ext = path.split('.')[-1].lower()
+    Loader = {'txt':TextLoader, 'pdf':PyPDFLoader}[ext]
+    loader = Loader(path)
+    docs = loader.load_and_split(text_splitter=text_splitter)
     
     vector_store = Chroma.from_documents(
         documents = docs,
@@ -50,6 +39,20 @@ def embed_file(file):
         persist_directory = './VectorDB',)
     retriever = vector_store.as_retriever()
     return retriever
+retriever = embed_file(file_path)
+
+query = "학칙 1장 1조는?"
+rel_docs = retriever.invoke(query)
+context = "\n".join([doc.page_content for doc in rel_docs])
+
+completion = client.chat.completions.create(
+    model="llama-3-korean-bllossom-8b",
+    temperature=0.7,
+    messages=[
+        {"role": "system", "content": f"학생들이 이해하기 쉽게 친절하게 설명해줘. 지식은 내가 준 문서에서만 가져와. 문장 끝날 때 이모티콘을 써줘,{context}"},
+        {"role": "user", "content": query}
+  ],
+)
         
 
 print(completion.choices[0].message.content)
